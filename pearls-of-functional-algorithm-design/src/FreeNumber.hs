e0b89dcb347ec4b6e9556dc92efaffaf87650ec1
Carlos Valiente <carlos.valiente@ecmwf.int>
2013-12-16 13:18:06 +0000

    Smallest free number: Reference implementation.

--- pearls-of-functional-algorithm-design/src/FreeNumber.hs
+++ pearls-of-functional-algorithm-design/src/FreeNumber.hs
@@ -1,12 +1,25 @@
 -- | Tools for computing the smallest natural number not in a given set.
 module FreeNumber
     (
-     minfree_ref
+      -- * Implementations
+      minfree_ref
+      -- * Properties
+    , prop_minfree_nonnegative
+    , prop_minfree_not_in_set
     ) where
-                   
 
--- | Reference implementation.
+-- | Returns the smallest natural number not present in the given set.
+--
+--  This is the reference implementation.
 minfree_ref    :: [Int] -> Int
 minfree_ref xs = head ([0 ..] \\ xs) where
     (\\) :: Eq a=> [a] -> [a] -> [a]
     us \\ vs = filter (`notElem` vs) us
+
+-- | The smallest natural number is a non-negative integer.
+prop_minfree_nonnegative      :: ([Int] -> Int) -> [Int] -> Bool
+prop_minfree_nonnegative f xs = (f xs) >= 0
+
+-- | The result is not in the given set
+prop_minfree_not_in_set      :: ([Int] -> Int) -> [Int] -> Bool
+prop_minfree_not_in_set f xs = (f xs) `notElem` xs
--- pearls-of-functional-algorithm-design/tests/FreeNumberTest.hs
+++ pearls-of-functional-algorithm-design/tests/FreeNumberTest.hs
@@ -1,7 +1,26 @@
 -- | Tests for the smallest free number tools.
 --
 -- Copied from https://github.com/jstolarek/haskell-testing-stub
+
 module Main where
 
+import Test.Framework (Test, defaultMain, testGroup)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+
+import FreeNumber
+
 main :: IO ()
-main = putStrLn "Not yet"
+main = defaultMain tests
+
+tests :: [Test]
+tests =
+      [
+       testGroup "Smallest free number"
+       [
+        testGroup "Reference implementation"
+        [
+          testProperty "Non-negative" (prop_minfree_nonnegative minfree_ref)
+         ,testProperty "Result not in set" (prop_minfree_not_in_set minfree_ref)
+        ]
+       ]
+      ]
