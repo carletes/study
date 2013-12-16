676b81312b34902a9a05aa712f936aef332e3f8d
Carlos Valiente <carlos.valiente@ecmwf.int>
2013-12-16 13:34:13 +0000

    Smallest free number: Simple benchmark

--- pearls-of-functional-algorithm-design/bench/FreeNumberBench.hs
+++ pearls-of-functional-algorithm-design/bench/FreeNumberBench.hs
@@ -3,5 +3,22 @@
 -- Copied from https://github.com/jstolarek/haskell-testing-stub
 module Main where
 
+import Criterion.Config (Config(..), defaultConfig, ljust)
+import Criterion.Main (Benchmark, bench, nf, defaultMainWith)
+
+import FreeNumber
+
+benchmarks :: [Benchmark]
+benchmarks =
+           [
+             bench "minfree_ref" $ nf minfree_ref [67 .. 100000]
+           ]
+
+config :: Config
+config = defaultConfig {
+           cfgPerformGC = ljust True
+         }
+
 main :: IO ()
-main = putStrLn "Not yet"
+main =
+    defaultMainWith config (return ()) benchmarks
